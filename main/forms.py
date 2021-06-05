e2618a267808618d66d6a8dfdcf990c13763783d
Misha2048 <mikaelan.itsmart@gmail.com>
2021-06-05 18:21:37 +0300

    Forms flly made

--- main/forms.py
+++ main/forms.py
@@ -1,22 +1,27 @@
 from django import forms
 from .models import *
+from django.utils.translation import ugettext_lazy as _
 
 ACTIONS = (
 	('Приём врача-кардиолога(с регистрацией и расшифровкой ЭКГ', 'Приём врача-кардиолога(с регистрацией и расшифровкой ЭКГ'),
-	('2', 'second'),
+	('Консультация врача-кардиологана дому у пациента (с регистрацией и расшифровкой ЭКГ)', 'Консультация врача-кардиологана дому у пациента (с регистрацией и расшифровкой ЭКГ)'),
+	('Регистрация и расшифровка ЭКГ', 'Регистрация и расшифровка ЭКГ'),
+	('Регистрация и расшифровка ЭКГ с ритмограммой', 'Регистрация и расшифровка ЭКГ с ритмограммой'),
+	('Суточное (холтеровское) мониторирование ЭКГ', 'Суточное (холтеровское) мониторирование ЭКГ'),
+	('Суточное мониторирование артериального давления', 'Суточное мониторирование артериального давления'),
 )
 
 DESTIONATIONS = (
-	('prm', 'Первичный'),
-	('sec', 'Повторный')
+	('Первичный', 'Первичный'),
+	('Повторный', 'Повторный')
 )
 
 class AppointmentForm(forms.Form):
-	name = forms.CharField(max_length = 100, widget=forms.TextInput(attrs={'class': 'myfieldclass'}))
-	age = forms.IntegerField(min_value=0, widget=forms.TextInput(attrs={'class': 'myfieldclass'}))
-	destination = forms.ChoiceField(choices=DESTIONATIONS, widget=forms.RadioSelect(attrs={'class' : 'myfieldclass'}))
-	phone = forms.CharField(max_length="11", widget=forms.TextInput(attrs={'class': 'myfieldclass'}))
-	action = forms.ChoiceField(choices=ACTIONS, widget=forms.Select(attrs={'class': 'myfieldclass'}))
+	name = forms.CharField(label = _("name"), max_length = 100, widget=forms.TextInput(attrs={'class': 'myfieldclass'}))
+	age = forms.IntegerField(label = _("age"), min_value=0, widget=forms.TextInput(attrs={'class': 'myfieldclass'}))
+	destination = forms.ChoiceField(label = _("destination"), choices=DESTIONATIONS, widget=forms.RadioSelect(attrs={'class' : 'myfieldclass'}))
+	phone = forms.CharField(label = _("phone"), max_length="11", widget=forms.TextInput(attrs={'class': 'myfieldclass'}))
+	action = forms.ChoiceField(label = _("action"), choices=ACTIONS, widget=forms.Select(attrs={'class': 'myfieldclass'}))
 
 # class AppointmentForm(forms.ModelForm):
 # 	class Meta:
