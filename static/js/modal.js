d536e89a0639e1da9b64efade90dea005c9acfa2
Ilya Kaplan <kaplan11092002@gmail.com>
2021-10-01 01:28:44 +0300

    backdrop click for modal added

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,16 +1,27 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    openModalBtnFixed: document.querySelector('[data-modal-open-fixed]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtnFixed: document.querySelector("[data-modal-open-fixed]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    backdrop: document.querySelector(".backdrop"),
   };
 
-  if(refs.openModalBtn) refs.openModalBtn.addEventListener('click', toggleModal);
-  if(refs.openModalBtnFixed) refs.openModalBtnFixed.addEventListener('click', toggleModal);
-  if(refs.closeModalBtn) refs.closeModalBtn.addEventListener('click', toggleModal);
+  if (refs.openModalBtn)
+    refs.openModalBtn.addEventListener("click", toggleModal);
+  if (refs.openModalBtnFixed)
+    refs.openModalBtnFixed.addEventListener("click", toggleModal);
+  if (refs.closeModalBtn)
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  if (refs.closeModalBtn)
+    refs.backdrop.addEventListener("click", backdropClick);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle("is-hidden");
+  }
+
+  function backdropClick(e) {
+    if (e.target !== e.currentTarget) return;
+    refs.modal.classList.toggle("is-hidden");
   }
 })();
