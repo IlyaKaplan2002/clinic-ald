f377799f796589b2c601bb8fcbc43ed491e42246
Ilya Kaplan <kaplan11092002@gmail.com>
2021-10-01 01:08:48 +0300

    mobile-menu-close button added

--- static/js/adaptive-menu.js
+++ static/js/adaptive-menu.js
@@ -1,8 +1,8 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className.includes("topnav") && !x.className.includes("responsive")) {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav nav-list-adaptive";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className.includes("topnav") && !x.className.includes("responsive")) {
+    x.className += " responsive";
+  } else {
+    x.className = "mobile-menu topnav nav-list-adaptive";
+  }
 }
